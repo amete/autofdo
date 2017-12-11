f242ec09af573759aa077793918f60951405c231
Alaettin Serhan Mete <amete@atpc001.cern.ch>
2017-12-11 19:59:25 +0100

    patch address_mapper

--- chromiumos-wide-profiling/address_mapper.cc
+++ chromiumos-wide-profiling/address_mapper.cc
@@ -4,23 +4,13 @@
 
 #include "chromiumos-wide-profiling/address_mapper.h"
 
-#include "base/logging.h"
+#include <stdint.h>
 
-#include "chromiumos-wide-profiling/limits.h"
 #include <vector>
 
+#include "base/logging.h"
+
 namespace quipper {
-
-AddressMapper::AddressMapper(const AddressMapper& source) {
-  mappings_ = source.mappings_;
-  real_address_map_ = source.real_address_map_;
-}
-
-bool AddressMapper::Map(const uint64_t real_addr,
-                        const uint64_t size,
-                        const bool remove_existing_mappings) {
-  return MapWithID(real_addr, size, kUint64Max, 0, remove_existing_mappings);
-}
 
 bool AddressMapper::MapWithID(const uint64_t real_addr,
                               const uint64_t size,
@@ -39,8 +29,7 @@ bool AddressMapper::MapWithID(const uint64_t real_addr,
   }
 
   // Check that this mapping does not overflow the address space.
-  if (real_addr + size - 1 != kUint64Max &&
-      !(real_addr + size > real_addr)) {
+  if (real_addr + size - 1 != UINT64_MAX && !(real_addr + size > real_addr)) {
     DumpToLog();
     LOG(ERROR) << "Address mapping at " << std::hex << real_addr
                << " with size " << std::hex << size << " overflows.";
@@ -50,101 +39,143 @@ bool AddressMapper::MapWithID(const uint64_t real_addr,
   // Check for collision with an existing mapping.  This must be an overlap that
   // does not result in one range being completely covered by another
   MappingList::iterator iter;
-  if (!mappings_.empty()) {
-    std::vector<MappedRange> mappings_to_delete;
-    bool old_range_found = false;
-    MappedRange old_range;
+  std::vector<MappingList::iterator> mappings_to_delete;
+  MappingList::iterator old_range_iter = mappings_.end();
+  for (iter = mappings_.begin(); iter != mappings_.end(); ++iter) {
+    if (!iter->Intersects(range))
+      continue;
+    // Quit if existing ranges that collide aren't supposed to be removed.
+    if (!remove_existing_mappings)
+      return false;
+    if (old_range_iter == mappings_.end() && iter->Covers(range)
+        && iter->size > range.size) {
+      old_range_iter = iter;
+      continue;
+    }
+    mappings_to_delete.push_back(iter);
+  }
 
-    iter = find(real_addr);
-    if (iter != mappings_.begin() && iter->second.real_addr > real_addr)
-      --iter;
-    for (;
-         iter != mappings_.end() && iter->second.real_addr < (real_addr + size);
-         ++iter) {
-      if (!iter->second.Intersects(range))
-        continue;
-      // Quit if existing ranges that collide aren't supposed to be removed.
-      if (!remove_existing_mappings)
+  for (MappingList::iterator mapping_iter : mappings_to_delete)
+    Unmap(mapping_iter);
+  mappings_to_delete.clear();
+
+  // Otherwise check for this range being covered by another range.  If that
+  // happens, split or reduce the existing range to make room.
+  if (old_range_iter != mappings_.end()) {
+    // Make a copy of the old mapping before removing it.
+    const MappedRange old_range = *old_range_iter;
+    Unmap(old_range_iter);
+
+    uint64_t gap_before = range.real_addr - old_range.real_addr;
+    uint64_t gap_after = (old_range.real_addr + old_range.size) -
+                         (range.real_addr + range.size);
+
+    // If the new mapping is not aligned to a page boundary at either its start
+    // or end, it will require the end of the old mapping range to be moved,
+    // which is not allowed.
+    if (page_alignment_) {
+      if ((gap_before && GetAlignedOffset(range.real_addr)) ||
+          (gap_after && GetAlignedOffset(range.real_addr + range.size))) {
+        LOG(ERROR) << "Split mapping must result in page-aligned mappings.";
         return false;
-      if (!old_range_found && iter->second.Covers(range) &&
-          iter->second.size > range.size) {
-        old_range_found = true;
-        old_range = iter->second;
-        continue;
       }
-      mappings_to_delete.push_back(iter->second);
     }
-    for (const MappedRange &R : mappings_to_delete)
-      CHECK(Unmap(R));
 
-    // Otherwise check for this range being covered by another range.  If that
-    // happens, split or reduce the existing range to make room.
-    if (old_range_found) {
-      CHECK(Unmap(old_range));
-
-      uint64_t gap_before = range.real_addr - old_range.real_addr;
-      uint64_t gap_after = (old_range.real_addr + old_range.size) -
-                           (range.real_addr + range.size);
-
-      if (gap_before) {
-        CHECK(MapWithID(old_range.real_addr,
-                        gap_before,
-                        old_range.id,
-                        old_range.offset_base,
-                        false));
+    if (gap_before) {
+      if (!MapWithID(old_range.real_addr, gap_before, old_range.id,
+                     old_range.offset_base, false)) {
+        LOG(ERROR) << "Could not map old range from " << std::hex
+                   << old_range.real_addr << " to "
+                   << old_range.real_addr + gap_before;
+        return false;
       }
-
-      CHECK(MapWithID(range.real_addr, range.size, id, offset_base, false));
-
-      if (gap_after) {
-        CHECK(MapWithID(range.real_addr + range.size,
-                        gap_after,
-                        old_range.id,
-                        old_range.offset_base + gap_before + range.size,
-                        false));
-      }
-      return true;
     }
+
+    if (!MapWithID(range.real_addr, range.size, id, offset_base, false)) {
+      LOG(ERROR) << "Could not map new range at " << std::hex << range.real_addr
+                 << " to " << range.real_addr + range.size << " over old range";
+      return false;
+    }
+
+    if (gap_after) {
+      if (!MapWithID(range.real_addr + range.size, gap_after, old_range.id,
+                     old_range.offset_base + gap_before + range.size, false)) {
+        LOG(ERROR) << "Could not map old range from " << std::hex
+                   << old_range.real_addr << " to "
+                   << old_range.real_addr + gap_before;
+        return false;
+      }
+    }
+
+    return true;
   }
 
   // Now search for a location for the new range.  It should be in the first
   // free block in quipper space.
 
+  uint64_t page_offset =
+      page_alignment_ ? GetAlignedOffset(range.real_addr) : 0;
+
   // If there is no existing mapping, add it to the beginning of quipper space.
   if (mappings_.empty()) {
-    range.mapped_addr = 0;
-    range.unmapped_space_after = kUint64Max - range.size;
-    insert(range);
+    range.mapped_addr = page_offset;
+    range.unmapped_space_after = UINT64_MAX - range.size - page_offset;
+    mappings_.push_back(range);
     return true;
   }
-  CHECK(!real_address_map_.empty());
 
   // If there is space before the first mapped range in quipper space, use it.
-  uint64_t first_mapped = real_address_map_.begin()->first;
-  if (first_mapped >= range.size) {
-    range.mapped_addr = 0;
-    range.unmapped_space_after = first_mapped - range.size;
-    insert(range);
+  if (mappings_.begin()->mapped_addr >= range.size + page_offset) {
+    range.mapped_addr = page_offset;
+    range.unmapped_space_after =
+        mappings_.begin()->mapped_addr - range.size - page_offset;
+    mappings_.push_front(range);
     return true;
   }
 
   // Otherwise, search through the existing mappings for a free block after one
   // of them.
-  MappingList::iterator first = mappings_.end();
-  uint64_t last_mapped_start = real_address_map_.rbegin()->first;
   for (iter = mappings_.begin(); iter != mappings_.end(); ++iter) {
-    if (iter->second.unmapped_space_after < size)
-      continue;
-    first = iter;
-    if (iter->second.mapped_addr != last_mapped_start)
-      break;
-  }
-  if (first != mappings_.end()) {
-    range.mapped_addr = first->second.mapped_addr + first->second.size;
-    range.unmapped_space_after =
-        first->second.unmapped_space_after - range.size;
-    first->second.unmapped_space_after = 0;
-    insert(range);
+    MappedRange& existing_mapping = *iter;
+    if (page_alignment_) {
+      uint64_t end_of_existing_mapping =
+          existing_mapping.mapped_addr + existing_mapping.size;
+
+      // Find next page boundary after end of this existing mapping.
+      uint64_t existing_page_offset = GetAlignedOffset(end_of_existing_mapping);
+      uint64_t next_page_boundary =
+          existing_page_offset
+              ? end_of_existing_mapping - existing_page_offset + page_alignment_
+              : end_of_existing_mapping;
+      // Compute where the new mapping would end if it were aligned to this
+      // page boundary.
+      uint64_t mapping_offset = GetAlignedOffset(range.real_addr);
+      uint64_t end_of_new_mapping =
+          next_page_boundary + mapping_offset + range.size;
+      uint64_t end_of_unmapped_space_after =
+          end_of_existing_mapping + existing_mapping.unmapped_space_after;
+
+      // Check if there's enough room in the unmapped space following the
+      // current existing mapping for the page-aligned mapping.
+      if (end_of_new_mapping > end_of_unmapped_space_after)
+        continue;
+
+      range.mapped_addr = next_page_boundary + mapping_offset;
+      range.unmapped_space_after =
+          end_of_unmapped_space_after - end_of_new_mapping;
+      existing_mapping.unmapped_space_after =
+          range.mapped_addr - end_of_existing_mapping;
+    } else {
+      if (existing_mapping.unmapped_space_after < range.size)
+        continue;
+      // Insert the new mapping range immediately after the existing one.
+      range.mapped_addr = existing_mapping.mapped_addr + existing_mapping.size;
+      range.unmapped_space_after =
+          existing_mapping.unmapped_space_after - range.size;
+      existing_mapping.unmapped_space_after = 0;
+    }
+
+    mappings_.insert(++iter, range);
     return true;
   }
 
@@ -159,22 +190,24 @@ bool AddressMapper::MapWithID(const uint64_t real_addr,
 void AddressMapper::DumpToLog() const {
   MappingList::const_iterator it;
   for (it = mappings_.begin(); it != mappings_.end(); ++it) {
-    LOG(INFO) << " real_addr: " << std::hex << it->second.real_addr
-              << " mapped: " << std::hex << it->second.mapped_addr
-              << " id: " << std::hex << it->second.id
-              << " size: " << std::hex << it->second.size;
+    LOG(INFO) << " real_addr: " << std::hex << it->real_addr
+              << " mapped: " << std::hex << it->mapped_addr
+              << " id: " << std::hex << it->id
+              << " size: " << std::hex << it->size;
   }
 }
 
 bool AddressMapper::GetMappedAddress(const uint64_t real_addr,
                                      uint64_t* mapped_addr) const {
   CHECK(mapped_addr);
-  MappingList::const_iterator iter = find(real_addr);
-  if (iter == mappings_.end() ||
-      !iter->second.ContainsAddress(real_addr))
-    return false;
-  *mapped_addr = iter->second.mapped_addr + real_addr - iter->second.real_addr;
-  return true;
+  MappingList::const_iterator iter;
+  for (iter = mappings_.begin(); iter != mappings_.end(); ++iter) {
+    if (!iter->ContainsAddress(real_addr))
+      continue;
+    *mapped_addr = iter->mapped_addr + real_addr - iter->real_addr;
+    return true;
+  }
+  return false;
 }
 
 bool AddressMapper::GetMappedIDAndOffset(const uint64_t real_addr,
@@ -182,70 +215,40 @@ bool AddressMapper::GetMappedIDAndOffset(const uint64_t real_addr,
                                          uint64_t* offset) const {
   CHECK(id);
   CHECK(offset);
-  MappingList::const_iterator iter = find(real_addr);
-  if (iter == mappings_.end() ||
-      !iter->second.ContainsAddress(real_addr))
-    return false;
-
-  *id = iter->second.id;
-  *offset = real_addr - iter->second.real_addr + iter->second.offset_base;
-  return true;
+  MappingList::const_iterator iter;
+  for (iter = mappings_.begin(); iter != mappings_.end(); ++iter) {
+    if (!iter->ContainsAddress(real_addr))
+      continue;
+    *id = iter->id;
+    *offset = real_addr - iter->real_addr + iter->offset_base;
+    return true;
+  }
+  return false;
 }
 
 uint64_t AddressMapper::GetMaxMappedLength() const {
   if (IsEmpty())
     return 0;
 
-  // The first mapped address
-  uint64_t min = real_address_map_.begin()->first;
-  // The real address start of the last mapped range
-  uint64_t last_real_start = real_address_map_.rbegin()->second;
-  auto iter = find(last_real_start);
-  uint64_t last_addr = iter->second.mapped_addr;
-  // The end of the last mapped range
-  uint64_t max = last_addr + iter->second.size;
+  uint64_t min = mappings_.begin()->mapped_addr;
+
+  MappingList::const_iterator iter = mappings_.end();
+  --iter;
+  uint64_t max = iter->mapped_addr + iter->size;
+
   return max - min;
 }
 
-bool AddressMapper::Unmap(const MappedRange &range) {
-
-  // Find the range to be removed
-  auto range_iter = mappings_.find(range.real_addr);
-  if (!range_iter->second.Matches(range))
-    return false;
-
-  // Find the range that preceeds this in mapped space.
-  auto iter = real_address_map_.find(range_iter->second.mapped_addr);
-  if (iter != real_address_map_.begin()) {
-    auto prev = iter;
-    --prev;
-    // update the corresponding range entry
-    auto prev_range_iter = mappings_.find(prev->second);
-        prev_range_iter->second.unmapped_space_after +=
-          range.size + range.unmapped_space_after;
+void AddressMapper::Unmap(MappingList::iterator mapping_iter) {
+  // Add the freed up space to the free space counter of the previous
+  // mapped region, if it exists.
+  if (mapping_iter != mappings_.begin()) {
+    const MappedRange& range = *mapping_iter;
+    MappingList::iterator previous_range_iter = std::prev(mapping_iter);
+    previous_range_iter->unmapped_space_after +=
+        range.size + range.unmapped_space_after;
   }
-  real_address_map_.erase(iter);
-  mappings_.erase(range_iter);
-  return true;
-}
-
-void AddressMapper::insert(const MappedRange &range) {
-  mappings_.emplace(range.real_addr, range);
-  real_address_map_.emplace(range.mapped_addr, range.real_addr);
-}
-
-AddressMapper::MappingList::iterator AddressMapper::find(uint64_t addr) {
-  auto iter = mappings_.upper_bound(addr);
-  if (iter != mappings_.begin())
-    --iter;
-  return iter;
-}
-AddressMapper::MappingList::const_iterator
-AddressMapper::find(uint64_t addr) const {
-  auto iter = mappings_.upper_bound(addr);
-  if (iter != mappings_.begin())
-    --iter;
-  return iter;
+  mappings_.erase(mapping_iter);
 }
 
 }  // namespace quipper
